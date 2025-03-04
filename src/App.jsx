51070ad9f5a5aaee33e7243ea3957b45b3679fdf
DogBrother <roys99141@gmail.com>
2025-03-04 14:20:52 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,77 +11,14 @@ import {
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const Home = () => {
-  return (
-      <div>
-          <ul>
-              <li>HOME</li>
-              <li>
-                  <Link to='/about'>About</Link>
-              </li>
-              <li>
-                  <Link to='/list'>List</Link>
-              </li>
-          </ul>
-      </div>
-  )
-}
-const About = () => {
-  const navigate = useNavigate()
-  const onClick = () => {
-      navigate('/')
-  }
-  return (
-      <div>
-          <button onClick={onClick}>BACK</button>
-      </div>
-  )
-}
-
-const Child1 = () => {
-  return <div>Child1</div>
-}
-const Child2 = () => {
-  return <div>Child2</div>
-}
-
-const List = () => {
-  return (
-      <div>
-          list 頁面
-          <Menus1 />
-          <Container />
-      </div>
-  )
-}
-const Container = () => {
-  return (
-      <div>
-          <Outlet />
-      </div>
-  )
-}
-const Menus1 = () => {
-  return (
-      <div>
-          <Link to={'/list/child1'}> one </Link>
-          <Link to={'/list/child2'}> two </Link>
-      </div>
-  )
-}
-
-const user = {
-  name: 'John Doe',
-  imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/120px-Google_Chrome_icon_%28February_2022%29.svg.png',
-  imageSize:90
-}
-
+import {Container} from 'react-bootstrap';
+import { Button,Stack } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <div className='App'>
       {/* <Router>
           <Routes>
@@ -93,37 +30,37 @@ function App() {
               </Route>
           </Routes>
       </Router> */}
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount(0)}>Reset</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Container fluid>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 2)}>
+            count is {count}
+          </button>
+          <button onClick={() => setCount(0)}>Reset</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="profile">
-        <h2>Profile</h2>
-        <ul>
-          <li>Name: {user.name}</li>
-          <img src={user.imageUrl} alt="" style={{
-            width: user.imageSize,
-            height: user.imageSize
-          }}/>
-        </ul>
-      </div>
+      </Container>
+      <Stack direction="horizontal" gap={3}>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </Stack>
     </div>
   )
 }
