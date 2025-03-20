d86d37b3b69287dad8629ae9a03a16c47a6230cc
DogBrother <roys99141@gmail.com>
2025-03-21 02:12:18 +0800

    工具列表首頁

---
+++ src/pages/tools/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ToolsIndex(){
+    return(
+        <>
+            <div>
+                <h1>工具</h1>
+                <p>工具列表</p>
+            </div>
+        </>
+    );
+}
+
+export default ToolsIndex;
