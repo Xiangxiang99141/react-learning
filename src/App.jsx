af27a0cb0344dda4ccad087806c344d1fc7df790
DogBrother <roys99141@gmail.com>
2025-03-21 02:11:35 +0800

    將路由移至外部檔案

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,24 @@ import React, { useState } from 'react'
 import {
   Routes,
   Route,
-  Link,
-  useNavigate,
-  Outlet,
-  useLocation,} from 'react-router-dom';
-// import { createBrowserHistory } from "history";
+  useRoutes,
+} from 'react-router-dom';
 import Header from './pages/header';
-import Home from './pages/Home';
-import About from './pages/About';
 import Footer from './pages/Footer';
 import { Container } from 'react-bootstrap';
-// import './App.css';
-
+import Tools from './router/Tools';
+import Main from './router/Main';
 function App() {
+  const allRoute = useRoutes([
+    ...Main,
+    ...Tools
+  ]);
 
   return (
     <>
       <Header />
       <Container>
-        <Routes>
-            <Route element={<Home />} path='/'></Route>
-            <Route element={<About />} path='/about'></Route>
-            {/* <Route element={<List />} path='/list'>
-                <Route element={<Child1 />} path='/list/child1'></Route>
-                <Route element={<Child2 />} path='/list/child2'></Route>
-            </Route> */}
-        </Routes>
+        {allRoute}
       </Container>
       <Footer />
     </>
