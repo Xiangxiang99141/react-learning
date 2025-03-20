71baef3bb766b4aa9f7d813e15ee456a33d951d7
DogBrother <roys99141@gmail.com>
2025-03-21 02:12:41 +0800

    主路由檔案

---
+++ src/router/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+const Main = [
+    {
+        name:'首頁',
+        path:'/',
+        element:<Home />,
+        fullpath:'/'
+    },
+    {
+        name:'關於我',
+        path:'/about',
+        element:<About />,
+        fullpath:'/about'
+    }
+];
+
+export default Main;
