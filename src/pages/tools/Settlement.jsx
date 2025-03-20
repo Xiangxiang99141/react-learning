742f15d1635c664ccbeda1e841dab28189a2aa79
DogBrother <roys99141@gmail.com>
2025-03-21 02:12:30 +0800

    結算表首頁

---
+++ src/pages/tools/Settlement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Settlement() {
+    return (
+        <div>
+            <h1>結算表</h1>
+            <p>這是一個結算表</p>
+        </div>
+    );
+}
+
+export default Settlement;
