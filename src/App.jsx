f05fe393aa6770c8e4f8e1f8a5d33a3a1cb0b7fd
DogBrother <roys99141@gmail.com>
2025-03-21 02:15:47 +0800

    更新錯字

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   Route,
   useRoutes,
 } from 'react-router-dom';
-import Header from './pages/header';
+import Header from './pages/Header';
 import Footer from './pages/Footer';
 import { Container } from 'react-bootstrap';
 import Tools from './router/Tools';
