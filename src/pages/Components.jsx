f0cecd61fff1454f91989a11897a9bb88ec7d168
DogBrother <roys99141@gmail.com>
2025-03-24 02:14:00 +0800

    componets首頁

---
+++ src/pages/Components.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Components(){
+    return(
+        <>
+            <div>
+                <h1>組件</h1>
+                <p>組建列表</p>
+            </div>
+        </>
+    );
+}
+
+export default Components;
